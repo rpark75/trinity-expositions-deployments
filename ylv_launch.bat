89b20f9564dda32bd5502a3c56a5ba03360d550b
rpark75 <sami.elhajoui@gmail.com>
2024-06-25 12:48:09 +0200

    Ajout class 0 detection

--- ylv_launch.bat
+++ ylv_launch.bat
@@ -2,4 +2,4 @@ echo Starting pattern interaction script. Copyright Lunacite.
 cd "C:\dev\trinity-expositions-deployments\yolov5"
 pip install -r requirements.txt 
 pip install websocket-client
-python detect.py --weights yolov5n.pt --nosave --source 1
+python detect.py --weights yolov5n.pt --nosave --class 0 --source 1
